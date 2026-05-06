e83fb3f31eec9d626b38845154d5950acdd69400
KrisztianVendegh <161598435+KrisztianVendegh@users.noreply.github.com>
2026-05-06 17:49:13 +0200

    dash+
    dashboard pripraveny na CRUD

--- Projekt_Vendégh/public/admin/dashboard.php
+++ Projekt_Vendégh/public/admin/dashboard.php
@@ -7,6 +7,15 @@ if (!isset($_SESSION['admin_logged_in'])) {
     exit;
 }
 
+require_once '../../app/classes/Database.php';
+require_once '../../app/classes/Article.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$articleModel = new Article($db);
+$articles = $articleModel->getAll();
+
 include '../../app/templates/header.php';
 ?>
 
@@ -14,13 +23,48 @@ include '../../app/templates/header.php';
 
 <section class="page-header">
     <div class="container">
-
         <h2>Admin Dashboard</h2>
+        <p>Vitaj, <?php echo $_SESSION['admin_username']; ?></p>
+    </div>
+</section>
 
-        <p>
-            Vitaj,
-            <?php echo $_SESSION['admin_username']; ?>
-        </p>
+<section class="admin-section">
+    <div class="container">
+
+        <div class="admin-top">
+            <h3>Zoznam článkov</h3>
+            <a class="admin-button" href="create.php">+ Pridať článok</a>
+        </div>
+
+        <div class="admin-table-wrapper">
+            <table class="admin-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Názov</th>
+                        <th>Model</th>
+                        <th>Vytvorené</th>
+                        <th>Akcie</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php foreach ($articles as $article): ?>
+                        <tr>
+                            <td><?php echo $article['id']; ?></td>
+                            <td><?php echo $article['title']; ?></td>
+                            <td><?php echo $article['model_key']; ?></td>
+                            <td><?php echo $article['created_at']; ?></td>
+                            <td>
+                                <a class="edit-link" href="edit.php?id=<?php echo $article['id']; ?>">Upraviť</a>
+                                <a class="delete-link" href="delete.php?id=<?php echo $article['id']; ?>">Vymazať</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+
+            </table>
+        </div>
 
     </div>
 </section>
